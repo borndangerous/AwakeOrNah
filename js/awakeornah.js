c4170f8b180dc33462bcf9f286344f3cecca48e1
Josh Trommel <joshtrommel@gmail.com>
2015-08-03 00:48:22 -0700

    [peter voice] lit

--- js/awakeornah.js
+++ js/awakeornah.js
@@ -1,6 +1,9 @@
 var adamResponse = get('http://is-adam-awake.herokuapp.com/');
 var adam = JSON.parse(adamResponse);
 
+var lastfmMoment = moment(adam.responses.lastfm.time, "DD MMM YYYY, HH:mm").subtract(8, 'hours')
+var githubMoment = moment(adam.responses.github, "YYYY MM DD HH:mm").subtract(8, 'hours')
+
 function get(url) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open('GET', url, false );
@@ -8,9 +11,9 @@ function get(url) {
     return xmlHttp.responseText;
 }
 
-var lastfmString = "He was listening to <i>" + adam.responses.lastfm.title
-                    + "</i> at <i>" + adam.responses.lastfm.time + "</i>";
-var githubString = "He last committed at " + adam.responses.github;
+var lastfmString = "He was listening to " + adam.responses.lastfm.title
+                    + " about " + lastfmMoment.fromNow()
+var githubString = "He commited something about " + githubMoment.fromNow();
 
 document.getElementById('lastfm').innerHTML = lastfmString;
 document.getElementById('github').innerHTML = githubString;
